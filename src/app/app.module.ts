c54ae5b0233d33c96fd62f1ad9686279ed881c9d
Oleksandr Fomenko <v-ofomenko@expediagroup.com>
2020-07-30 19:16:52 +0300

    Added product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first/first-component/first-component.component';
-import { ProductComponent } from './product/product.component';
+import { ProductComponent } from './products/components/product/product.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/products/components/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onBuy() {
+    console.log('You were purchased a product');
+  }
+
+}
